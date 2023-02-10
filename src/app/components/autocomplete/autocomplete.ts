71ceaec61a3c04c080a33adc50c377581a4e71dd
Sergey Vohmyanin <delagen@gmail.com>
2023-02-10 20:04:30 +0300

    fix(autocomplete): use overlayOptions instead of virtualScrollOptions for p-overlay

--- src/app/components/autocomplete/autocomplete.ts
+++ src/app/components/autocomplete/autocomplete.ts
@@ -138,7 +138,7 @@ export const AUTOCOMPLETE_VALUE_ACCESSOR: any = {
             <p-overlay
                 #overlay
                 [(visible)]="overlayVisible"
-                [options]="virtualScrollOptions"
+                [options]="overlayOptions"
                 [target]="'@parent'"
                 [appendTo]="appendTo"
                 [showTransitionOptions]="showTransitionOptions"
